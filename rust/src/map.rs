1990b13d795ee0a6dfee1d32e67ef5d5f1229cb1
Jikolin <plahotya.ivan@gmail.com>
2026-04-29 21:01:41 +0200

    Edited map code: no custom directions and coords, only Vector2i now; glogalised Directions

--- rust/src/map.rs
+++ rust/src/map.rs
@@ -1,261 +1,225 @@
 use godot::prelude::*;
-use godot::classes::{ GridMap, MeshLibrary, Area3D };
+use godot::classes::{ GridMap, MeshLibrary };
 
 use rand::RngExt;
 
-use crate::player::Player;
+use crate::utils::{assets, Dir2i, load_resource};
 
 
 #[derive(Clone, Debug)]
 enum CellType { 
-	Wall, 
-	Room, 
-	Bridge { direction: Direction }, 
-	Start 
+    Wall, 
+    Room, 
+    Bridge { direction: Vector2i }, 
+    Start 
 }
-
-enum CellState { Unused, Used, OutOfGrid }
-
 
 #[derive(Clone)]
 struct CellData {
-	enter: Direction,
-	doors: [Direction; 4],
-}
-
-
-#[derive(Clone, Debug)]
-struct Coords {
-	y: usize,
-	x: usize
-}
-
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-struct Direction {
-	y: isize,
-	x: isize,
-}
-impl Direction {
-	const UP: Direction  = Direction { y: -1, x: 0 };
-	const RIGHT: Direction = Direction { y: 0, x: 1 };
-	const DOWN: Direction  = Direction { y: 1, x: 0 };
-	const LEFT: Direction  = Direction { y: 0, x: -1 };
-	const NULL: Direction = Direction { y: 0, x: 0};
-
-	fn all() -> [Self; 4] {
-		[Self::UP, Self::RIGHT, Self::DOWN, Self::LEFT]
-	}
+    enter: Vector2i,
+    doors: [Vector2i; 4],
 }
 
 #[derive(Clone)]
 struct Cell {
-	c_type: CellType,
-	data: Option<CellData>
+    c_type: CellType,
+    data: Option<CellData>
 }
 
 #[derive(Clone)]
 struct PossCoords {
-	bridge: Coords,
-	room: Coords,
-	direction: Direction
+    bridge: Vector2i,
+    room:   Vector2i,
+    direction: Vector2i
 }
 
 
 #[derive(GodotClass)]
 #[class(base=Node, no_init)]
 pub struct MapLayer {
-	base: Base<Node>,
-	width: usize,
-	height: usize,
-	s_coords: Coords,
-	grid: Vec<Vec<Cell>>,
+    base:     Base<Node>,
+    width:    usize,
+    height:   usize,
+    s_coords: Vector2i,
+    grid:     Vec<Vec<Cell>>,
 }
 
 
 #[godot_api]
 impl MapLayer {
-	pub fn new(width: usize, height: usize) -> Gd<Self> {
-		let mut map = Gd::from_init_fn(|base| {
-			Self {
-				base,
-				width,
-				height,
-				s_coords: Coords{
-					y: rand::rng().random_range(0..height),
-					x: rand::rng().random_range(0..width) 
-				},
-				grid: vec![vec![Cell{c_type: CellType::Wall, data: None}; width]; height],
-			}
-		});
-		map.bind_mut().generate();
-		map
-	}
+    pub fn new(width: usize, height: usize) -> Gd<Self> {
+        let mut map = Gd::from_init_fn(|base| {
+            Self {
+                base,
+                width,
+                height,
+                s_coords: Vector2i {
+                    x: rand::rng().random_range(1..width  as i32 - 1),
+                    y: rand::rng().random_range(1..height as i32 - 1),
+                },
+                grid: vec![vec![Cell { c_type: CellType::Wall, data: None }; width]; height],
+            }
+        });
+        map.bind_mut().generate();
+        map
+    }
 
-
-	#[func]
+    #[func]
     pub fn print_grid(&self) {
         for row in &self.grid {
             for cell in row {
                 match cell.c_type {
-                    CellType::Wall => print!("#"),
-                    CellType::Room => print!("@"),
-                    CellType::Bridge { direction: _ } => print!("-"),
-                    CellType::Start => print!("S"),
+                    CellType::Wall           => print!("#"),
+                    CellType::Room           => print!("@"),
+                    CellType::Bridge { .. }  => print!("-"),
+                    CellType::Start          => print!("S"),
                 }
             }
             println!();
         }
     }
 
+    #[func]
+    pub fn generate(&mut self) {
+        self.generate_labyrinth();
+    }
 
     #[func]
-	pub fn generate(&mut self) {
-		self.generate_labyrinth();
-	}
+    pub fn build_grid_map(&self) -> Gd<GridMap> {
+        let mut grid_map = GridMap::new_alloc();
+        let mesh_lib = load_resource::<MeshLibrary>(assets::LABYRINTH_MESH_LIB);
+        grid_map.set_cell_size(Vector3::new(1.0, 0.4, 1.0));
+        grid_map.set_mesh_library(&mesh_lib);
 
+        for y in 0..self.height {
+            for x in 0..self.width {
+                match self.get_cell_type(Vector2i::new(x as i32, y as i32)) {
+                    Some(CellType::Room) | Some(CellType::Start) => {
+                        grid_map.set_cell_item(Vector3i::new(x as i32, 0, y as i32), 0);
+                    },
+                    Some(CellType::Bridge { direction }) => {
+                        let orientation = match direction {
+                            Dir2i::UP | Dir2i::DOWN   => 16,
+                            Dir2i::RIGHT | Dir2i::LEFT => 10,
+                            _ => 0
+                        };
+                        grid_map.set_cell_item_ex(Vector3i::new(x as i32, 1, y as i32), 1)
+                            .orientation(orientation)
+                            .done();
+                    },
+                    _ => {}
+                }
+            }
+        }
+        grid_map
+    }
 
-	#[func]
-	pub fn build_grid_map(&self) -> Gd<GridMap> {
-		let mut grid_map = GridMap::new_alloc();
-		grid_map.set_cell_size(Vector3::new(1.0, 0.4, 1.0));
-		// grid_map.set_position(Vector3::new(0.0, 0.0, 0.0));
-		let mesh_lib: Gd<MeshLibrary> = load("res://assets/labyrinth.tres");
-		grid_map.set_mesh_library(&mesh_lib);
-		for y in 0..self.height {
-			for x in 0..self.width {
-				match self.get_cell_type(&Coords{y, x}) {
-					CellType::Room => grid_map.set_cell_item(Vector3i::new(x as i32, 0, y as i32), 0),
-					CellType::Bridge { direction } => {
-						// Needed if bridge isn't square like
-						let orientation = match direction {
-						    Direction::UP | Direction::DOWN => 16,
-						    Direction::RIGHT | Direction::LEFT => 10,
-						    _ => 0
-						};
-						// Setting default parameters
-						grid_map.set_cell_item_ex(Vector3i::new(x as i32, 1, y as i32), 1)
-						.orientation(orientation)
-						.done();
-					},
-					_ => {}
-				}
-			}
-		}
-		grid_map
-	}
+    #[func]
+    pub fn is_walkable(&self, coords: Vector2i) -> bool {
+        if !self.coords_is_ok(coords) {
+            return false;
+        }
+        matches!(self.get_cell_type(coords), Some(CellType::Room) | Some(CellType::Start) | Some(CellType::Bridge { .. }))
+    }
 
-
-	#[func]
-	pub fn is_walkable(&self, coords: Vector2i) -> bool {
-		if coords.x < 0 || coords.y < 0 {
-			return false;
-		}
-		let coords = Coords{ y: coords.y as usize, x: coords.x as usize };
-		matches!(self.get_cell_state(&coords), CellState::Used)
-	}
-
-	#[func]
-	pub fn get_start_position(&self) -> Vector3 {
-		Vector3::new( self.s_coords.x as f32 + 0.5, 1.0, self.s_coords.y as f32 + 0.5 )
-	}
+	// Player-oriented
+    #[func]
+    pub fn get_start_position(&self) -> Vector3 {
+        Vector3 {
+            x: self.s_coords.x as f32 + 0.5,
+            y: 1.0,
+            z: self.s_coords.y as f32 + 0.5,
+        }
+    }
 }
 
 
-// Map Generating logic
 impl MapLayer {
+    fn coords_is_ok(&self, coords: Vector2i) -> bool {
+        coords.x >= 0 && coords.y >= 0
+        && coords.x < self.width  as i32
+        && coords.y < self.height as i32
+    }
 
-	fn get_cell_type(&self, coords: &Coords) -> CellType {
-		self.grid[coords.y][coords.x].c_type.clone()
-	}
+    fn get_cell_type(&self, coords: Vector2i) -> Option<CellType> {
+        if self.coords_is_ok(coords) {
+            Some(self.grid[coords.y as usize][coords.x as usize].c_type.clone())
+        } else {
+            None
+        }
+    }
 
-	fn get_mut_cell(&mut self, coords: &Coords) -> &mut Cell {
-		&mut self.grid[coords.y][coords.x]
-	}
+    fn get_mut_cell(&mut self, coords: Vector2i) -> Option<&mut Cell> {
+        if self.coords_is_ok(coords) {
+            Some(&mut self.grid[coords.y as usize][coords.x as usize])
+        } else {
+            None
+        }
+    }
 
-	fn get_mut_cell_data(&mut self, coords: &Coords) -> &mut Option<CellData> {
-		&mut self.grid[coords.y][coords.x].data
-	}
+    fn get_mut_cell_data(&mut self, coords: Vector2i) -> Option<&mut CellData> {
+        if self.coords_is_ok(coords) {
+            self.grid[coords.y as usize][coords.x as usize].data.as_mut()
+        } else {
+            None
+        }
+    }
 
-	fn set_cell(&mut self, coords: &Coords, c_type: CellType) {
-		self.grid[coords.y][coords.x].c_type = c_type;
-	}
+    fn set_cell(&mut self, coords: Vector2i, c_type: CellType) {
+        if self.coords_is_ok(coords) {
+            self.grid[coords.y as usize][coords.x as usize].c_type = c_type;
+        }
+    }
 
+    fn get_cell_state(&self, coords: Vector2i) -> bool {
+        matches!(self.get_cell_type(coords), Some(CellType::Wall))
+    }
 
-	fn get_cell_state(&self, coords: &Coords) -> CellState {
-		if coords.y >= self.height || coords.x >= self.width {
-			return CellState::OutOfGrid;
-		}
-		match self.get_cell_type(&coords) {
-			CellType::Wall => return CellState::Unused,
-			_ => return CellState::Used
-		}
-	}
+    fn check_direction(&self, coords: Vector2i, dir: Vector2i) -> Option<Vector2i> {
+        let new_coords = Vector2i {
+            x: coords.x + dir.x,
+            y: coords.y + dir.y,
+        };
+        if self.get_cell_state(new_coords) {
+            Some(new_coords)
+        } else {
+            None
+        }
+    }
 
-	fn check_direction(&self, coords: &Coords, dir: &Direction) -> Option<Coords> {
-		let new_coords = Direction{y: coords.y as isize + dir.y,
-								   x: coords.x as isize + dir.x};
-		if new_coords.y < 0 || new_coords.x < 0 {
-			return None;
-		}
+    fn get_poss_coords(&self, coords: Vector2i) -> Vec<PossCoords> {
+        let mut poss_coords = vec![];
+        for dir in Dir2i::all() {
+            if let Some(bridge) = self.check_direction(coords, dir) {
+                if let Some(room) = self.check_direction(bridge, dir) {
+                    poss_coords.push(PossCoords { bridge, room, direction: dir });
+                }
+            }
+        }
+        poss_coords
+    }
 
-		let new_coords = Coords{y: new_coords.y as usize, 
-								x: new_coords.x as usize};
-		match self.get_cell_state(&new_coords) {
-			CellState::Unused => return Some(new_coords),
-			_ => return None
-		}
-	}
+    fn generate_labyrinth(&mut self) {
+        let mut curr_coords = self.s_coords;
+        self.set_cell(curr_coords, CellType::Start);
+        let mut created_cells: Vec<Vector2i> = vec![curr_coords];
 
-	fn get_poss_coords(&self, coords: &Coords) -> Vec<PossCoords> {
-		let mut poss_coords: Vec<PossCoords> = vec![];
-		for dir in Direction::all() {
-			if let Some(new_coords_1) = self.check_direction(&coords, &dir) {
-				if let Some(new_coords_2) = self.check_direction(&new_coords_1, &dir) {
-					poss_coords.push(PossCoords{
-						bridge: new_coords_1,
-						room: new_coords_2,
-						direction: dir
-					});
-				}
-			}
-		}
-		poss_coords
-	}
-
-	fn get_near_bridges(&self, coords: &Coords) -> [Direction; 4] {
-		let mut bridges = [Direction::NULL; 4];
-		let mut count = 0;
-		for dir in Direction::all() {
-			if let Some(new_coords) = self.check_direction(coords, &dir) {
-				bridges[count] = dir;
-				count += 1;
-			}
-		}
-		bridges
-	}
-
-	fn generate_labyrinth(&mut self) {
-		let mut curr_coords = self.s_coords.clone();
-		self.set_cell(&curr_coords, CellType::Start);
-		let mut created_cells: Vec<Coords> = vec![];
-
-		for _i in 0..30 {
-			let way_lenght: usize = rand::rng().random_range(1..5);
-			for _i in 0..way_lenght {
-				let poss_coords = self.get_poss_coords(&curr_coords);
-				if poss_coords.is_empty() {
-					let indx = rand::rng().random_range(0..created_cells.len());
-					curr_coords = created_cells[indx].clone();
-					continue;
-				}
-				let indx = rand::rng().random_range(0..poss_coords.len());
-				let new_coords = poss_coords[indx].clone();
-				self.set_cell(&new_coords.bridge, CellType::Bridge{ direction: new_coords.direction });
-				self.set_cell(&new_coords.room, CellType::Room);
-				curr_coords = new_coords.room.clone();
-				created_cells.push(curr_coords.clone());
-			}
-		}
-	}
+        for _ in 0..30 {
+            let way_length: usize = rand::rng().random_range(1..5);
+            for _ in 0..way_length {
+                let poss_coords = self.get_poss_coords(curr_coords);
+                if poss_coords.is_empty() {
+                    let indx = rand::rng().random_range(0..created_cells.len());
+                    curr_coords = created_cells[indx];
+                    continue;
+                }
+                let indx = rand::rng().random_range(0..poss_coords.len());
+                let next = &poss_coords[indx];
+                self.set_cell(next.bridge, CellType::Bridge { direction: next.direction });
+                self.set_cell(next.room, CellType::Room);
+                curr_coords = next.room;
+                created_cells.push(curr_coords);
+            }
+        }
+    }
 }
--- rust/src/player.rs
+++ rust/src/player.rs
@@ -4,50 +4,39 @@ use godot::classes::{
  	CollisionShape3D, MeshInstance3D, Input
 };
 
-
 use crate::map::MapLayer;
-use crate::utils::{assets, load_scene, load_inst_as};
-
-
-struct Direction;
-impl Direction {
-    const UP: Vector3    = Vector3 { x: 0.0, y: 0.0, z: -1.0 };
-    const RIGHT: Vector3 = Vector3 { x: 1.0, y: 0.0, z: 0.0 };
-    const DOWN: Vector3  = Vector3 { x: 0.0, y: 0.0, z: 1.0 };
-    const LEFT: Vector3  = Vector3 { x: -1.0, y: 0.0, z: 0.0 };
-}
+use crate::utils::{assets, load_scene, load_scene_as, Dir3};
 
 
 #[derive(GodotClass)]
 #[class(base=CharacterBody3D, no_init)]
 pub struct Player {
 	base: Base<CharacterBody3D>,
+	mesh: Gd<MeshInstance3D>,
+	shape: Gd<CollisionShape3D>,
 	map: Gd<MapLayer>,
-	// mesh: Gd<MeshInstance3D>,
-	// collision: Gd<CollisionShape3D>,
 
-	is_moving: bool,
+	is_moving: 		bool,
 	is_in_the_room: bool,
 
 	step_speed: f32,
-	step_size: f32,
+	step_size:  f32,
 	walk_speed: f32,
 
 	target_velocity: Vector3,
-	target_pos: Vector3,
-	target_rot: Basis,	
+	target_pos: 	 Vector3,
+	target_rot: 	 Basis,	
 }
 
 
 #[godot_api]
 impl ICharacterBody3D for Player {
 	fn ready(&mut self) {
-		let mesh = load_inst_as::<MeshInstance3D>(assets::PLAYER_MESH);
-		let shape = load_inst_as::<CollisionShape3D>(assets::PLAYER_SHAPE);
+		let mesh = self.mesh.clone();
+		let shape = self.shape.clone();
 
-		let map = self.map.clone();
-
-		self.base_mut().set_position(map.bind().get_start_position());
+		let start_position = self.map.bind().get_start_position();
+		self.base_mut().set_position(start_position);
 		self.target_pos = self.base().get_position();
 
 		self.base_mut().add_child(&mesh);
@@ -77,25 +66,23 @@ impl ICharacterBody3D for Player {
 #[godot_api]
 impl Player {
 	pub fn new(map: Gd<MapLayer>) -> Gd<Self> {
-		// let model = load::<PackedScene>("res://scenes/player.tscn")
-		// 	.instantiate_as::<Node3D>();
-
 		Gd::from_init_fn(|base| {
 			Self {
 				base,
+				mesh:  load_scene_as::<MeshInstance3D>(assets::PLAYER_MESH),
+				shape: load_scene_as::<CollisionShape3D>(assets::PLAYER_SHAPE),
 				map,
-				// model,
 
-				is_moving: false,
+				is_moving: 		false,
 				is_in_the_room: false,
 
 				step_speed: 5.0,
-				step_size: 2.0,
+				step_size: 	2.0,
 				walk_speed: 3.5,
 
 				target_velocity: Vector3::ZERO,
-				target_pos: Vector3::ZERO,
-				target_rot: Basis::default(),
+				target_pos: 	 Vector3::ZERO,
+				target_rot: 	 Basis::default(),
 			}
 		})
 	}
@@ -108,13 +95,13 @@ impl Player {
 
         } else {
         	if input.is_action_just_pressed("ui_up").into() {
-	        	self.try_to_move(Direction::UP);
+	        	self.try_to_move(Dir3::UP);
 	        } else if input.is_action_just_pressed("ui_right").into() {
-	        	self.try_to_move(Direction::RIGHT);
+	        	self.try_to_move(Dir3::RIGHT);
 	        } else if input.is_action_just_pressed("ui_down").into() {
-	        	self.try_to_move(Direction::DOWN);
+	        	self.try_to_move(Dir3::DOWN);
 	        } else if input.is_action_just_pressed("ui_left").into() {
-	        	self.try_to_move(Direction::LEFT);
+	        	self.try_to_move(Dir3::LEFT);
 	        }
 
 	        if input.is_action_just_pressed("interact").into() {
